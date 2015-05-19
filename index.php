c3e5dda84fa9557a34931d5c1ebfa05c1051897c
LinnH <linnbast@hotmail.com>
2015-05-19 14:14:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	</head>
 	
 	<body>
-        <?php include("processreg.php")?>
+        <?php include("processreg.php") ?>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<div id="container">
 			<header>
