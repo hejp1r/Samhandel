5fd54e71cbe1c04eb011dd7df6ee432bfc40199b
LinnH <linnbast@hotmail.com>
2015-05-19 12:47:43 +0200

    Update recept.php

--- recept.php
+++ recept.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<title>SamHandla mera</title>
-		<link rel="stylesheet" href= "main.css">
-		<script src="main.js"></script>
-	</head>
-	
-	<body>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<div id="container">
-			<header>
-				<h2>Samhandla</h2>
-			<div class = "loggain">
-			<input type="submit" name="minalistor" id="minalistor" value="Mina listor" />
-			<input type="submit" name="recept" id="recept" value="Söka recept" action="recept.php" />
-			<input type="submit" name="dagar" id="dagar" value="SPECIAL DAYS" />
-			<input type="submit" name="loggaut" id="loggaut" value="Logga ut" />
-			</div>
-			</header>
-			<div id="banner">
-			</div>
-			<div id="content2">
-			<br/>
-			<fieldset>
-			<legend> Välkommen att söka recept här</legend>
-			</fieldset>
-			<b/> 
-	  	</div>	
-			<div id="content">	
-			</div>
-		</div>	
-		</body>
-		</html>
+<?php
+header('Location:file:///C:/Users/Linn/Documents/GitHub/Samhandel/');
+?>
