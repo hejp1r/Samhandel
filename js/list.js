ef8dd255fea99e4dc1c1e1d0400cff4ca274bc45
hejp1r <p.larsson.89@gmail.com>
2015-05-26 22:23:46 +0200

    listooor

--- js/list.js
+++ js/list.js
@@ -4,10 +4,15 @@ $(document).ready(function (){
     $('#nylista').click(function(){
         var length = getLength();
        // console.log(length);
+        if($('#listnamn').val()==''){
+           $('#listnamn').val('Inköpslista #' + (length+1));
+        }
         var list = createList(length);
         $('#box').append(list);
        // console.log($('#box'));
+        $('#listnamn').val("");
         })
+    
     $(document).on("click", '.addp', function(e){
     //    console.log($('#list1'));
       //  console.log($('#btn1'));
@@ -23,23 +28,51 @@ $(document).ready(function (){
 
     })
     
-    $(document).on("click", '.sparalista', function(e){
+    $(document).on("click", '.btnShow', function(e){
         
+        var num = getId($(this));
+        $('.p'+num+'').toggle('active');
+        $('#btnEdit' + num + '').toggle('slow');
+        $('#btnDeleteList' + num + '').toggle('slow');
         
+        //$('#btnX' + id + '').toggleClass('show');
+   /*         function(){
+                $(this).removeClass('active');
+                var num = getId($(this));
+                $('#btnX' + num + '').hide();
+            })
+            
+       */ 
+    })
+    $('document').on("click", '.btnDeleteList', function(e){
+        alert("sääääker?");
     })
     
+    $(document).on("click", '.btnEdit', function(e){
+        
+        var num = getId($(this));
+        
+        $('.p' + num + '').toggleClass('active');
+        $('.btnX' + num + '').show();
+        
+    })
+    function getId(obj){
+        console.log(obj);
+        var id = obj.attr('id');
+        var num = id.substr(id.length - 1);
+        return num;
+    }
     function getLength(){
         return $('.listclass').length+1;
     }
     function createList(length){
-        return  "<div class='listclass' id='list"+length+"' name='list"+length+"'>"
-        //        +   "<form action='page.php'>"
-                +       "<input type='text' placeholder='Ny produkt' id='prod"+length+"' name='prod"+length+"'>"
-                +       "<input type='submit' value='+' class='addp' id='btn"+length+"'>" //med en sluttagg här så förstörs allt, WTF?!
-       //         +   "</form><br>"
-                +   "<form action='../processes/processlist.php' method='POST'>"
-                +       "<input type='hidden' value='list"+length+"' name='list'>"
-                +       "<input type='submit' value='sparalista' class='save' id='save"+length+"' name='save"+length+"'>"
+        return  "<div class='listclass' id='list" + length + "' name='list" + length + "'>"
+                +      "<h2>" + $('#listnamn').val() + "</h1>"
+                +      "<input type='text' placeholder='Ny produkt' id='prod" + length + "' name='prod" + length + "'>"
+                +      "<input type='submit' value='+' class='addp' id='btn" + length + "'>" //med en sluttagg här så förstörs allt, WTF?!
+                +      "<form action='../processes/processlist.php' method='POST'>"
+                +          "<input type='hidden' value='" + $('#listnamn').val() + "' name='list'>"
+                +          "<input type='submit' value='sparalista' class='save' id='save" + length + "' name='save" + length + "'>"
                 +   "</form>"
                 +"</div>";    
     }
@@ -50,3 +83,11 @@ $(document).ready(function (){
 //dataaabaasen. Efter att man skrivit upp allt man vill ha köpt så finns det en knapp som heter spara. Vad den gör är att den då tar alla innehållet i alla inputs, skickar den till servern som där packar ihop den till en query och sen skickas till dbn. När man hämtar det så får php processa det och skriva ut det fint i html, när det uppdateras så uppdateras HELA listan, inte ett litet objektet. Den gamla listan raderas alltså och den nya förs in.
 
 //listid    lista       användare
+
+//listan skapas men tilldelas ett autoincrementat id av db, value för 'name' kommer istället att vara det namn som användaren döper
+//listan till igenom ett input som i dbn = listName, listan Måste döpas annars, blir värdet 'inköpslista'.
+
+
+//en lista raderas genom att det den ingår i ett form som innehåller en gömd input med listid'et. När man klickar på knappen 'radera lista' submitas den till servern som där tar hand om det.
+
+//En lista redigeras igenom att inputen man klickar bort, addas till det gömda formet runt knappen ändra, om man redigerar det så kommer värdet läggas till och när man submitar postas det till servern.
---
+++ processes/deletelist.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+if(isset($_SESSION['user'])){
+    echo "Welcome " . $_SESSION['user'];
+}
+else{
+    echo "logga in helvete";
+}
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "samhandla";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else{
+    echo "upprättad kontakt";
+}
+$list = $_POST['list'];
+
+function deleteList($conn, $list){
+    $sql = "DELETE FROM list WHERE listId='$list'";
+    $query = "DELETE FROM produkt WHERE listId='$list'";
+    
+    if($conn->query($sql) === true && $conn->query($query) === true){
+        echo "listan är booorta";
+        header('Location: ../pages/page.php');
+    } else{
+        echo "va i helvete ". $conn->error;
+    }
+}
+deleteList($conn, $list);
